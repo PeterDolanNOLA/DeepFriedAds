56219ad2810618a7b35cff100f58ece485ebfd13
PETER DOLAN <peterdolan@gmail.com>
2019-10-22 16:09:00 -0500

    added js file

---
+++ index.js
@@ -0,0 +1,62 @@
+const obj = {
+    "jrDeveloper": {
+      "cardGroups": [
+        {
+          "title": "Card Title",
+          "image": "https://deepfried.com/wp-content/uploads/2014/05/astroboy-1600x1000.jpg",
+          "link": "https://deepfried.com"
+        },
+        {
+          "title": "Another Card Title Living in the Middle",
+          "image": "https://deepfried.com/wp-content/uploads/2014/05/Device-Mockup.png",
+          "link": "https://deepfried.com/job/developer/"
+        },
+        {
+          "title": "Title of a Card",
+          "image": "https://deepfried.com/wp-content/uploads/2016/01/nola-patio.jpg",
+          "link": "https://deepfried.com/featured-brands/"
+        },
+        {
+          "title": "Location & Hours",
+          "image": "",
+          "link": "https://deepfried.com/contact-us/location-hours/"
+        },
+        {
+          "title": "Deep Fried Ipsum",
+          "image": "https://deepfried.com/wp-content/uploads/2014/10/french-fries.jpg",
+          "link": "https://deepfried.com/now-introducing-deep-fried-ipsum/"
+        },
+        {
+          "title": "Meet the Family",
+          "image": "https://deepfried.com/wp-content/uploads/2014/06/ampersand.jpg",
+          "link": "https://deepfried.com/about-us/meet-the-family/"
+        }
+      ]
+    }
+  }
+
+
+ let cards = obj["jrDeveloper"]["cardGroups"];
+
+ cards.forEach((card)=> {
+     let cardBoxDiv = document.createElement("div");
+     cardBoxDiv.classList.add('card-box');
+     
+     
+      let cardBoxLabelDiv = document.createElement("div");
+      cardBoxLabelDiv.classList.add('card-box-label');
+        
+      let labelContainerDiv = document.createElement("div");
+      labelContainerDiv.classList.add('label-text-container');
+
+      let labelText = document.createElement("h2");
+      labelTextNode = document.createTextNode(card.title);
+      labelText.appendChild(labelTextNode);
+      labelContainerDiv.appendChild(labelText);
+      cardBoxLabelDiv.appendChild(labelContainerDiv);
+      cardBoxDiv.appendChild(cardBoxLabelDiv);
+      let container = document.getElementById('card-container');
+      console.log(container);
+    
+
+  })
